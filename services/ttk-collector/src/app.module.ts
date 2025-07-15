070b28f1cdd142ff4660027ea9314483205836cb
David Zaitsev <davidgalanix@gmail.com>
2025-07-15 22:01:42 +0300

    add prisma service to ttk collector

--- services/ttk-collector/src/app.module.ts
+++ services/ttk-collector/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NatsService } from './nats/nats.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, NatsService],
+  providers: [AppService, NatsService, PrismaService],
 })
 export class AppModule {}
--- services/ttk-collector/src/app.service.ts
+++ services/ttk-collector/src/app.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { NatsService } from './nats/nats.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService implements OnModuleInit {
-  constructor(private readonly natsService: NatsService) {}
+  constructor(
+    private readonly natsService: NatsService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async onModuleInit() {
     await this.natsService.connect();
 
     this.natsService.subscribe('event.tiktok', (data) => {
-      console.log('[NATS message, tiktok]', data);
+      // this.prisma.event;
     });
   }
 }
