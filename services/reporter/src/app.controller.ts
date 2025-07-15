fe3b59fa85444ddc8966f6d82f23dd9e21956bb2
David Zaitsev <davidgalanix@gmail.com>
2025-07-15 13:29:54 +0300

    add all new changes to start project

---
+++ services/reporter/src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  eventHandle(@Body() body: any) {
+    console.log(body);
+  }
+}
