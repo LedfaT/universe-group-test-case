ac1543f651fcd935edbc918ed7270b04b609f8cc
David Zaitsev <davidgalanix@gmail.com>
2025-07-16 10:50:11 +0300

    make collectors saving data

--- services/fb-collector/src/app.service.ts
+++ services/fb-collector/src/app.service.ts
@@ -1,15 +1,84 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { NatsService } from './nats/nats.service';
-
+import { PrismaService } from './prisma/prisma.service';
+import { Event, FacebookEvent } from 'types/eventTypes';
 @Injectable()
 export class AppService implements OnModuleInit {
-  constructor(private readonly natsService: NatsService) {}
+  constructor(
+    private readonly natsService: NatsService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async onModuleInit() {
     await this.natsService.connect();
 
-    this.natsService.subscribe('event.facebook', (data) => {
-      console.log('[NATS message, facebook]', data);
+    this.natsService.subscribe('event.facebook', async (msg: string) => {
+      const eventData: FacebookEvent = JSON.parse(msg);
+      console.log(eventData);
+
+      const { user, engagement } = eventData.data;
+
+      let parsedGender: 'male' | 'female' | 'non_binary';
+
+      switch (user.gender) {
+        case 'male':
+        case 'female':
+          parsedGender = user.gender;
+          break;
+        case 'non-binary':
+          parsedGender = 'non_binary';
+          break;
+        default:
+          throw new Error(`Invalid gender value: ${user.gender}`);
+      }
+
+      const createdUser = await this.prisma.facebookUser.create({
+        data: {
+          userId: user.userId,
+          name: user.name,
+          age: user.age,
+          gender: parsedGender,
+          country: user.location.country,
+          city: user.location.city,
+        },
+      });
+
+      let engagementTopId: string | null = null;
+      let engagementBottomId: string | null = null;
+
+      if ('actionTime' in engagement) {
+        const createdEngagement =
+          await this.prisma.facebookEngagementTop.create({
+            data: {
+              ...engagement,
+            },
+          });
+        engagementTopId = createdEngagement.id;
+      }
+
+      if ('campaignId' in engagement) {
+        const createdEngagement =
+          await this.prisma.facebookEngagementBottom.create({
+            data: {
+              ...engagement,
+            },
+          });
+        engagementBottomId = createdEngagement.id;
+      }
+
+      await this.prisma.event.create({
+        data: {
+          eventId: eventData.eventId,
+          timestamp: eventData.timestamp,
+          source: 'facebook',
+          funnelStage: eventData.funnelStage,
+          eventType: eventData.eventType,
+
+          facebookUserId: createdUser.id,
+          fbEngagementTopId: engagementTopId,
+          fbEngagementBottomId: engagementBottomId,
+        },
+      });
     });
   }
 }
--- services/ttk-collector/src/app.service.ts
+++ services/ttk-collector/src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { NatsService } from './nats/nats.service';
 import { PrismaService } from './prisma/prisma.service';
-
+import { Event, TiktokEvent, TiktokUser } from 'types/eventTypes';
 @Injectable()
 export class AppService implements OnModuleInit {
   constructor(
@@ -12,8 +12,52 @@ export class AppService implements OnModuleInit {
   async onModuleInit() {
     await this.natsService.connect();
 
-    this.natsService.subscribe('event.tiktok', (data) => {
-      // this.prisma.event;
+    this.natsService.subscribe('event.tiktok', async (msg: string) => {
+      const eventData: TiktokEvent = JSON.parse(msg);
+
+      const { user, engagement } = eventData.data;
+
+      const createdUser = await this.prisma.tiktokUser.create({
+        data: {
+          ...user,
+        },
+      });
+
+      let engagementTopId: string | null = null;
+      let engagementBottomId: string | null = null;
+
+      if ('watchTime' in engagement) {
+        const createdEngagement = await this.prisma.tiktokEngagementTop.create({
+          data: {
+            ...engagement,
+          },
+        });
+        engagementTopId = createdEngagement.id;
+      }
+
+      if ('actionTime' in engagement) {
+        const createdEngagement =
+          await this.prisma.tiktokEngagementBottom.create({
+            data: {
+              ...engagement,
+            },
+          });
+        engagementBottomId = createdEngagement.id;
+      }
+
+      await this.prisma.event.create({
+        data: {
+          eventId: eventData.eventId,
+          timestamp: eventData.timestamp,
+          source: 'tiktok',
+          funnelStage: eventData.funnelStage,
+          eventType: eventData.eventType,
+
+          tiktokUserId: createdUser.id,
+          ttEngagementTopId: engagementTopId,
+          ttEngagementBottomId: engagementBottomId,
+        },
+      });
     });
   }
 }
