edeeb9c05cbb7652c87874f9f993e57bd135ed35
David Zaitsev <davidgalanix@gmail.com>
2025-07-25 00:09:30 +0300

    increase max messages count in stream

--- services/gateway/src/nats/nats.service.ts
+++ services/gateway/src/nats/nats.service.ts
@@ -32,7 +32,7 @@ export class NatsService implements OnModuleInit, OnModuleDestroy {
         subjects: ['event.>'],
         retention: RetentionPolicy.Workqueue,
         storage: StorageType.File,
-        max_msgs: 1_500_000,
+        max_msgs: 5_000_000,
         max_bytes: 2 * 1024 * 1024 * 1024,
         max_age: 60 *30 * 1_000_000_000,
         discard: DiscardPolicy.Old,                      
